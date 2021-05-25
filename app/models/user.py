aa7304ea20a57a692c261e2a9b8bb69183f7e0f8
Caleb <cpbeachler@gmail.com>
2021-05-25 19:32:01 -0400

    fixed integer location

--- app/models/user.py
+++ app/models/user.py
@@ -9,7 +9,7 @@ class User(db.Model, UserMixin):
   username = db.Column(db.String(40), nullable = False, unique = True)
   email = db.Column(db.String(255), nullable = False, unique = True)
   hashed_password = db.Column(db.String(255), nullable = False)
-  location = db.Column(db.String(255))
+  location = db.Column(db.Integer, nullable = False)
 
 
   @property
